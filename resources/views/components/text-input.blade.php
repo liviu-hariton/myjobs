2cf565184b201dd9998dd5b052902735452a6c58
Liviu Hariton <liviu.hariton@gmail.com>
2023-10-09 10:59:32 +0300

    Added Alpine.js

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,6 +1,6 @@
 <div {{ $attributes->merge(['class' => 'input-group mb-3']) }}>
-    <input type="text" class="form-control" value="{{ $value }}" name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}">
-    @if($formId)
-        <a href="#" class="input-group-text text-decoration-none" onclick="document.getElementById('{{ $name }}').value='';document.getElementById('{{ $formId }}').submit();return false;"><i class="fa fa-times"></i></a>
+    <input x-ref="input-{{ $name }}" type="text" class="form-control" value="{{ $value }}" name="{{ $name }}" id="input-{{ $name }}" placeholder="{{ $placeholder }}">
+    @if($formRef)
+        <a href="javascript:void(0)" class="input-group-text text-decoration-none" @click="$refs['input-{{ $name }}'].value='';$refs['{{ $formRef }}'].submit();"><i class="fa fa-times"></i></a>
     @endif
 </div>
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,20 +1,20 @@
 <x-layout>
     <x-breadcrumbs :section="'jobs'" />
 
-    <div class="card mb-3 bg-white">
+    <div class="card mb-3 bg-white" x-data="">
         <div class="card-body">
-            <form method="get" action="{{ route('jobs.index') }}" id="filtering-form">
+            <form x-ref="filters" method="get" action="{{ route('jobs.index') }}" id="filtering-form">
                 <div class="row">
                     <div class="col-6">
                         <span class="form-text">Search</span>
-                        <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for a job title" form-id="filtering-form" />
+                        <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for a job title" form-ref="filters" />
                     </div>
                     <div class="col-6">
                         <span class="form-text">Salary</span>
 
                         <div class="d-flex flex-row">
-                            <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" class="me-2" form-id="filtering-form" />
-                            <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" form-id="filtering-form" />
+                            <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" class="me-2" form-ref="filters" />
+                            <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" form-ref="filters" />
                         </div>
                     </div>
                     <div class="col-6 mt-2">
