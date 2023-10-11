6dd94d79e5593aeb5aea324ef743cd4c163bdb97
Liviu Hariton <liviu.hariton@gmail.com>
2023-10-11 22:36:10 +0300

    Display input errors

--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -11,13 +11,19 @@
                     @csrf
 
                     <div class="input-group mb-3">
-                        <input type="number" class="form-control" placeholder="Your expected salary" name="expected_salary" id="expected_salary">
+                        <input type="number" @class(['form-control', 'is-invalid' => $errors->has('expected_salary')]) placeholder="Your expected salary" name="expected_salary" id="expected_salary" value="{{ old('expected_salary') }}">
                         <span class="input-group-text">EUR</span>
                     </div>
+                    @error('expected_salary')
+                    <p><span class="badge text-bg-danger">{{ $message }}</span></p>
+                    @enderror
 
                     <div class="mb-3">
                         <label for="cv" class="form-label">Upload your resume</label>
-                        <input class="form-control" type="file" id="cv" name="cv">
+                        <input @class(['form-control', 'is-invalid' => $errors->has('cv')]) type="file" id="cv" name="cv">
+                        @error('cv')
+                        <p><span class="badge text-bg-danger">{{ $message }}</span></p>
+                        @enderror
                     </div>
 
                     <button type="submit" class="btn btn-outline-success w-100 mt-2"><i class="fa fa-check-circle"></i> Apply</button>
